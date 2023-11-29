77087412dfd47bd713f83af9927e526321d31aa7
Nirmal1995 <nirmalg786@gmail.com>
2023-11-29 20:20:43 +0000

    Nirmal:app.py

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__)   
+posts = [
+    {
+        'title': 'Book 1',
+        'Writer': 'Author 1',
+        'Content': 'Content1'
+    },
+    {
+        'title': 'Book 2',
+        'Writer': 'Author 2',
+        'Content': 'Content 2'
+    }
+]
 @app.route('/')
+@app.route('/home')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', posts=posts)
 if __name__ == '__main__':
     app.run(debug=True)
