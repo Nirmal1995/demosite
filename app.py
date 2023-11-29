15b52ce3aa4d08612c08e7b84aee5b75553e666f
Nirmal1995 <nirmalg786@gmail.com>
2023-11-29 00:03:06 +0000

    change2

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+def get_data():
+    data = ["Item1", "Item2", "Item3"]
+    return data 
 @app.route('/')
 def index():
-    data = ["I1"]
+    data = get_data()
     return render_template('index.html', data=data)
 if __name__ == '__main__':
     app.run(debug=True)
