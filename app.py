43c8684d7fcf252481296f27deeffa52493d0869
Nirmal1995 <nirmalg786@gmail.com>
2023-11-29 14:58:12 +0000

    Nirmal: App.py

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-def get_data():
-    data = ["Item1", "Item2", "Item3"]
-    return data 
 @app.route('/')
 def index():
     return render_template('index.html')
