db7e340f9092f99b0d7d89d7aff2a6ccc0d8f79e
kripal-gupta8 <krupal.gupta8@gmail.com>
2023-12-11 15:05:36 +0000

    updating routes.py

--- routes.py
+++ routes.py
@@ -12,3 +12,10 @@ class Userstore(db.Model):
     uname = db.Column(db.String(20))
     password = db.Column(db.String(20))
     date_created = db.Column(db.DateTime, default=datetime.now)
+
+class Patients(db.Model):
+    __tablename__ = 'patients'
+    id = db.Column(db.Integer, primary_key=True)
+    ssn_id = db.Column(db.Integer)
+    pname = db.Column(db.String(20), nullable=False)
+    age = db.Column(db.Integer)
