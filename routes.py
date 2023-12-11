0bd8255430353a7ca3e4948db09af7b2f80e2f21
kripal-gupta8 <krupal.gupta8@gmail.com>
2023-12-11 15:01:59 +0000

    updating routes.py

--- routes.py
+++ routes.py
@@ -5,3 +5,10 @@ from sqlalchemy import Column, Integr, ForeignKey
 from sqlalchemy.orm import sessionmaker, relationship
 from datetime import datetime, date
 import re
+
+class Userstore(db.Model):
+    __tablename__ = 'userstore'
+    id = db.Column(db.Integer, primary_key=True)
+    uname = db.Column(db.String(20))
+    password = db.Column(db.String(20))
+    date_created = db.Column(db.DateTime, default=datetime.now)
