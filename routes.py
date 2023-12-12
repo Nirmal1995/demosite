57da3dfdf5aa049e483f91d2a22c16bee8220594
kripal-gupta8 <krupal.gupta8@gmail.com>
2023-12-12 14:26:42 +0000

    Updating the medicine inventory by implementing deletemed api where authorized users are able to view and delete existing medicine records from the database

--- routes.py
+++ routes.py
@@ -461,3 +461,22 @@ def updatemed():
         flash('Logged out! Please login again to continue')
         return redirect( url_for('login') )
     return render_template('updatemed.html')
+
+@app.route('/deletemed')
+def deletemed():
+    if 'username' in session:
+        usern = session['username']
+        print(usern)
+        updatep = MedicineMaster.query.all()
+        print(updatep)
+        if not updatep:
+            flash('Record not found!')
+            return redirect( url_for('addMedicine') )
+        else:
+            print("inside else")
+            return render_template('deletemed.html', updatep = updatep)
+
+    else:
+        flash('Logged out! Please login again to continue')
+        return redirect( url_for('login') )
+    return render_template('deletemed.html')
